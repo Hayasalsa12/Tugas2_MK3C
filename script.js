e38a620913364f7f8a3074fed18a105a8833a1e3
Hayasalsa12 <541251138@student.smktelkom-pwt.sch.id>
2026-08-27 15:22:40 +0700

    tambah js

---
+++ script.js
@@ -0,0 +1,33 @@
+function cekRegister() {
+
+    let nama = document.getElementById("nama_lengkap").value;
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let konfirmasiPassword = document.getElementById("konfirmasi-password").value;
+    let kelas = document.getElementById("kelas").value;
+
+    if (nama === "" || username === "" || email === "" || password === "") {
+        alert("Data penting harus diisi!");
+    } else if (password.length < 8) {
+        alert("Password minimal 8 karakter!");
+    } else if (password !== konfirmasiPassword) {
+        alert("Password dan konfirmasi password tidak sama!");
+    } else if (kelas === "") {
+        alert("Silakan pilih kelas!");
+    } else {
+        alert("Data registrasi valid!");
+    }
+}
+function cekLogin() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "" || password === "") { 
+        alert("Username dan password wajib diisi!");
+    } else {
+        alert("Data login sudah lengkap!");
+    }
+}
+
